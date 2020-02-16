56851ea02a9fb9f101c2e8fa9320c730104f0a9f
Oriol Soriano <oriolsoriano@gmail.com>
2020-02-16 19:29:12 +0100

    update key: Handle the case in which the configmap does not exist

--- test/acceptance/update-key.js
+++ test/acceptance/update-key.js
@@ -1,21 +1,20 @@
 const uuid = require('uuid/v4');
 const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
 chai.should();
+chai.use(chaiAsPromised);
 
 const k8sClientFactory = require('../../lib/factories/k8sClient');
 const KeyValueK8s = require('../..');
 
+
 Feature('Update key', () => {
-  const k8sClient = k8sClientFactory();
-  const key = `test-configmap-${uuid()}`;
-
-  afterEachScenario(async () => {
-    await k8sClient.deleteNamespacedConfigMap(key, 'default')
-  });
-
   Scenario('Existing key', () => {
+    const k8sClient = k8sClientFactory();
+    const key = `test-configmap-${uuid()}`;
+
     Given('a configmap named ${key}', async () => {
-      const configmap = await k8sClient.createNamespacedConfigMap(
+      await k8sClient.createNamespacedConfigMap(
         'default',
         {
           'data': {'message': 'All your base are belong to us!'},
@@ -33,6 +32,25 @@ Feature('Update key', () => {
       const updatedConfigmap = await k8sClient.readNamespacedConfigMap(key, 'default')
 
       updatedConfigmap.body.data.should.deep.equal({'answer': '42'});
+    });
+
+    after( async () => {
+      await k8sClient.deleteNamespacedConfigMap(key, 'default')
+    });
+  });
+
+  Scenario('Non-existent key', () => {
+    let nonexistantKey;
+
+    Given('a non-existant configmap', () => {
+      nonexistantKey = `nonexistant-${uuid()}`;
+    });
+
+    When('attempting to update the value of the key', () => {});
+
+    Then('a "not found" exception is raised', async () => {
+      const keyvalueK8s = new KeyValueK8s();
+      keyvalueK8s.updateKey(nonexistantKey, {'a': '42'}).should.eventually.throw(/not found/);
     });
   });
 });
